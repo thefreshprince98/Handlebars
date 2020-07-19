2aa5779f6d914634db8a4c5c7d61360a6efef39d
thefreshprince98 <58113441+thefreshprince98@users.noreply.github.com>
2020-07-19 13:59:36 -0400

    this is what i have

---
+++ database/seeds.sql
@@ -0,0 +1,5 @@
+USE `burgers_db`;
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Angus', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Turkey', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Veggie', false, CURRENT_TIMESTAMP);
